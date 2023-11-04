40f837a82b4b823ea8dd51656c7a2e2c93244dc9
KacyUmana <118031268+KacyUmana@users.noreply.github.com>
2023-11-04 00:28:41 -0700

    changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -28,4 +28,10 @@ public class ArrayTests {
     int[] input1 = {1, 2, 3, 4};
     assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {1, 2, 3};
+    assertEquals(2.5, ArrayExamples.averageWithoutLowest(input1), 0.001);
+  }
 }
---
+++ ListTests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTests {
+    @Test
+    public void testFilter() {
+      
+    }
+    
+    @Test
+    public void testMerge() {
+
+    }
+}
